57c03f08ba602ed87e772e51594d8d304ffec860
kirandesimone <kirandesimone@yahoo.com>
2020-02-20 14:54:35 -0500

    add vendoMaticCLI and grabbing stock.txt

---
+++ module-1/capstone/VendoMatic/src/main/java/com/techelevator/StockDAO.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+
+public class StockDAO {
+	
+	private final File ITEM_STOCK_FILE = new File("C:\\Users\\Student\\workspace\\java-module-1-capstone-team-4\\module-1\\capstone\\java\\vendingmachine.csv");
+	
+	public StockDAO() {
+		
+	}
+	
+	public void getStockInfoFromFile() {
+		
+	}
+	
+}
---
+++ module-1/capstone/VendoMatic/src/main/java/com/techelevator/VendoMaticCLI.java
@@ -0,0 +1,17 @@
+package com.techelevator;
+
+public class VendoMaticCLI {
+
+	
+	public VendoMaticCLI() {
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
