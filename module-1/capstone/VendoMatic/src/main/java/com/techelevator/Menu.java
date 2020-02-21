95a82306b10685ad0dff75b618b73afac8ff5914
kirandesimone <kirandesimone@yahoo.com>
2020-02-21 13:48:20 -0500

    display purchase and exit

--- module-1/capstone/VendoMatic/src/main/java/com/techelevator/Menu.java
+++ module-1/capstone/VendoMatic/src/main/java/com/techelevator/Menu.java
@@ -1,7 +1,31 @@
 package com.techelevator;
 
+import java.util.Scanner;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 
 public class Menu {
+	private Scanner in;
+	private PrintWriter out;
+	
+	
+	public Menu(InputStream input, OutputStream output) {
+		this.in = new Scanner(input);
+		this.out = new PrintWriter(output);
+		
+	}
+	
+	public void displayMenuOptions(Object[] object) {
+		for(int i = 0; i < object.length; i++) {
+			int optionNum = i + 1;
+			System.out.println(optionNum + ") " + object[i]);
+		}
+	}
+	
+	
+	
+	
 
 }
--- module-1/capstone/VendoMatic/src/main/java/com/techelevator/VendoMaticCLI.java
+++ module-1/capstone/VendoMatic/src/main/java/com/techelevator/VendoMaticCLI.java
@@ -5,20 +5,30 @@ import java.io.FileNotFoundException;
 public class VendoMaticCLI {
 	private static final String MAIN_MENU_ITEM_DISPLAY = "******************* ITEM DISPLAY ******************";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	private static final String MAIN_MENU_OPTION_EXIT =	"Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
 	
-	private static StockDAO stock = new StockDAO();
+	//private static StockDAO stock = new StockDAO();
+	private Menu menu;
 	
-	public VendoMaticCLI() {
-		
+	public void run() {
+		while(true) {
+			StockDAO stockList = new StockDAO();
+			
+			System.out.println(MAIN_MENU_ITEM_DISPLAY + "\n");
+			stockList.getProductList();
+			
+			if()
+		}
+	}
+	
+	public VendoMaticCLI(Menu menu) {
+		this.menu = menu;
 	}
 	
 	
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println(MAIN_MENU_ITEM_DISPLAY + "\n");
-		
-		stock.getStockInfoFromFile();
-		stock.getProductList();
 		
 		
 	}
