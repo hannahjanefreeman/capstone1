2aa3d0f3773be4bc9dc49bb6906851aac4a6ee9a
Hannah Freeman <hannahjanefreeman@gmail.com>
2020-02-22 15:35:51 -0500

    Created log class and new log.txt, created text string for output

---
+++ module-1/capstone/VendoMatic/src/main/java/com/techelevator/LogClass.java
@@ -0,0 +1,31 @@
+package com.techelevator;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class LogClass {
+	
+	private File log = new File ("C:\\Users\\Student\\workspace\\java-module-1-capstone-team-4\\module-1\\capstone\\java\\log.txt");
+
+		
+	public void transacationLog(String uniqueID, String name, String price, double wallet) throws FileNotFoundException {
+		
+		PrintWriter logJawn = new PrintWriter(new FileOutputStream(log), true);
+		
+		LocalDate date = LocalDate.now();
+		LocalTime time = LocalTime.now();
+		
+			String printDate = date.toString();
+			String printTime = time.toString();
+			
+			logJawn.println(printDate + " " + printTime + " " + name + " $" + price + " $" + wallet);
+		}
+
+}
