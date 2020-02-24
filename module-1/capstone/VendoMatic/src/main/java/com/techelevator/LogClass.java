9c58bbbef0902dbc4cf8895973fe6bd2cc491387
Hannah Freeman <hannahjanefreeman@gmail.com>
2020-02-24 15:49:13 -0500

    Date and Time fixed in LogClass

--- module-1/capstone/VendoMatic/src/main/java/com/techelevator/LogClass.java
+++ module-1/capstone/VendoMatic/src/main/java/com/techelevator/LogClass.java
@@ -1,18 +1,16 @@
 package com.techelevator;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class LogClass {
 	
-	private File log = new File ("C:\\Users\\Student\\workspace\\java-module-1-capstone-team-4\\module-1\\capstone\\java", "log.txt");
+	private File log = new File ("C:\\Users\\Student\\workspace\\java-module-1-capstone-team-4\\module-1\\capstone\\VendoMatic", "log.txt");
 
 	public LogClass() {
 		
@@ -21,12 +19,12 @@ public class LogClass {
 	public void transacationLog(String itemSelection, double price, double wallet) throws FileNotFoundException {
 		try(PrintWriter logJawn = new PrintWriter(new FileOutputStream(log, true))) {
 			LocalDate date = LocalDate.now();
-			LocalTime time = LocalTime.now();
-			
-				String printDate = date.toString();
-				String printTime = time.toString();
+			LocalDateTime time = LocalDateTime.now();
+			String pattern = "hh:mm:ss a";
+			String printDate = date.toString();
+			String printTime = time.format(DateTimeFormatter.ofPattern(pattern));
 				
-				logJawn.println(printDate + " " + printTime + " " + itemSelection + " $" + price + " $" + wallet);
+			logJawn.println(printDate + " " + printTime + " " + itemSelection + " $" + price + " $" + wallet);
 		}
 	}
 }
